10b20eaf86521d9d62f954ad343fc0eed0c973d1
trigaux <trigaux@cisco.com>
2023-06-14 18:24:42 -0700

    Being fancy, because why not.

--- include/transport/stream_id.h
+++ include/transport/stream_id.h
@@ -5,20 +5,19 @@
 
 namespace qtransport {
   class StreamId {
-    using type = uint64_t;
-    friend struct std::atomic<StreamId>;
+    using value_type = uint64_t;
 
   public:
     StreamId() = default;
     ~StreamId() = default;
 
-    constexpr StreamId(type value, bool is_server, bool is_unidirectional)
+    constexpr StreamId(value_type value, bool is_server, bool is_unidirectional)
         : _is_server{is_server},
           _is_unidirectional{is_unidirectional} {
       adjust(value);
     }
 
-    constexpr StreamId(type value)
+    constexpr StreamId(value_type value)
         : _value{value},
           _is_server{value & 0b01},
           _is_unidirectional{value & 0b10} {}
@@ -28,13 +27,13 @@ namespace qtransport {
 
     constexpr StreamId &operator=(const StreamId &) = default;
     constexpr StreamId &operator=(StreamId &&) = default;
-    constexpr StreamId &operator=(const type &value) {
+    constexpr StreamId &operator=(const value_type &value) {
       _is_server = value & 0b01;
       _is_unidirectional = value & 0b10;
       adjust(value);
       return *this;
     }
-    constexpr StreamId &operator=(type &&value) {
+    constexpr StreamId &operator=(value_type &&value) {
       _is_server = value & 0b01;
       _is_unidirectional = value & 0b10;
       _value = std::move(value);
@@ -42,9 +41,7 @@ namespace qtransport {
       return *this;
     }
 
-    constexpr operator type() const {
-      return _value;
-    }
+    constexpr operator value_type() const { return _value; }
 
     constexpr StreamId &operator++() noexcept {
       adjust(_value + 4);
@@ -58,30 +55,31 @@ namespace qtransport {
     }
 
   private:
-    constexpr void adjust(type value) {
+    constexpr void adjust(value_type value) {
       _value = (value & (~0x0u << 2)) | _is_server | (_is_unidirectional * 2);
     }
 
   private:
-    type _value : 62;
-    type _is_server : 1;
-    type _is_unidirectional : 1;
+    value_type _value : 62;
+    value_type _is_server : 1;
+    value_type _is_unidirectional : 1;
   };
 }// namespace qtransport
 
 template<>
 class std::atomic<qtransport::StreamId> : std::__atomic_base<qtransport::StreamId> {
-  using base_atomic = std::__atomic_base<qtransport::StreamId>;
+  using value_type = qtransport::StreamId;
+  using base_type = std::__atomic_base<value_type>;
 
 public:
-  constexpr atomic(qtransport::StreamId value) noexcept : base_atomic(value) {}
+  constexpr atomic(value_type value) noexcept : base_type(value) {}
   constexpr atomic(const atomic &) = delete;
   constexpr atomic(atomic &&) = delete;
   constexpr atomic &operator=(const atomic &) = delete;
   constexpr atomic &operator=(atomic &&) = delete;
 
   template<typename... Args>
-  constexpr atomic(Args... args) noexcept : base_atomic(qtransport::StreamId(std::forward<Args>(args)...)) {}
+  constexpr atomic(Args... args) noexcept : base_type(value_type(std::forward<Args>(args)...)) {}
 
   atomic &operator++() {
     auto v = load();
@@ -96,5 +94,5 @@ public:
     return v_out;
   }
 
-  constexpr operator qtransport::StreamId() const { return load(); }
+  constexpr operator value_type() const { return load(); }
 };
